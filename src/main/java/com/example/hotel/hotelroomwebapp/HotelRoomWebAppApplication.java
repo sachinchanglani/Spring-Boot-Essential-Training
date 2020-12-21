22590baf1248e1da932d94acd64f7ab662ba462d
Sachin <sachinchanglani@gmail.com>
2020-12-21 18:48:40 +0530

    First Commit

---
+++ src/main/java/com/example/hotel/hotelroomwebapp/HotelRoomWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.hotel.hotelroomwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelRoomWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelRoomWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hotel/hotelroomwebapp/HotelRoomWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotel.hotelroomwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelRoomWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
